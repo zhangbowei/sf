18d9ad272c273c572ff6ed03011841f77504785a
ZhangBowei <919270881@qq.com>
2017-04-08 09:25:55 +0800

    add user-Agent

--- makeLink/makeLink.py
+++ makeLink/makeLink.py
@@ -54,6 +54,7 @@ class BDTB:
             # 构建URL
             url = self.baseURL + self.seeLZ + str(pageNum)
             request = urllib2.Request(url)
+            request.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.8.1.14) Gecko/20080404 (FoxPlus) Firefox/2.0.0.14');
             response = urllib2.urlopen(request)
             # 返回UTF-8格式编码内容
             return response.read().decode('utf-8')
